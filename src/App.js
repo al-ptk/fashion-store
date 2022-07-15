4a8f8e402271e5b44271314cd715d282a6fb4e99
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-15 17:34:59 -0300

    Set up i18next files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useTranslation } from 'react-i18next';
 import './App.css';
 
 function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div className="App">
       <h1>Fashion Store</h1>
+      <h2>{t('TestTitle')}</h2>
+      <p>
+        <button onClick={() => i18n.changeLanguage('en')}>en</button>
+        <button onClick={() => i18n.changeLanguage('pt')}>pt</button>
+      </p>
     </div>
   );
 }
