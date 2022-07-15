6ec04aaceea1fdc8c0fa93940b3fdd43be0db2f7
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-15 17:39:50 -0300

    Update readme

--- src/App.js
+++ src/App.js
@@ -5,12 +5,6 @@ function App() {
   const { t, i18n } = useTranslation();
   return (
     <div className="App">
-      <h1>Fashion Store</h1>
-      <h2>{t('TestTitle')}</h2>
-      <p>
-        <button onClick={() => i18n.changeLanguage('en')}>en</button>
-        <button onClick={() => i18n.changeLanguage('pt')}>pt</button>
-      </p>
     </div>
   );
 }
