9d547bb048d2b0b2c842fdcfd0c6a91b5a692054
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-16 11:53:56 -0300

    Add folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import { useTranslation } from 'react-i18next';
 import './App.css';
 
 function App() {
-  const { t, i18n } = useTranslation();
   return (
     <div className="App">
     </div>
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationEN from './locales/en/translationEN.json';
-import translationPT from './locales/pt/translationPT.json';
+import translationEN from '../locales/en/translationEN.json';
+import translationPT from '../locales/pt/translationPT.json';
 
 const resources = {
   en: {
