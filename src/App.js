51c8fb68e931c2e939efb3a98356e29cef78ed60
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-22 15:08:26 -0300

    Add GlobalStyle and theme files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
+import { ThemeProvider } from 'styled-components';
+import theme from './utils/styled/theme';
+import GlobalStyle from './utils/styled/GlobalStyle';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Normalize />
-      <Routes>
-        <Route path='/' element={<div>hey there</div>}/>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Normalize />
+        <GlobalStyle />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>Hello there!</h1>
+                <p>General Kenobi!</p>
+              </div>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
