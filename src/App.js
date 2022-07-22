97052271b2887ba49373ac4f847bd35b5f256ea2
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-22 16:21:21 -0300

    Clean up for HeroIntro branch

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ import { ThemeProvider } from 'styled-components';
 import theme from './utils/styled/theme';
 import GlobalStyle from './utils/styled/GlobalStyle';
 
-import styled from 'styled-components';
-
-const MockHome = () => {
-  return (
-    <div>
-      <h1 style={{ fontFamily: 'Oleo Script Swash Caps' }}>Brittania</h1>
-      <p style={{ fontFamily: 'Playfair Display' }}>Featured</p>
-      <p style={{ fontFamily: 'Playfair Display', fontWeight: 700 }}>
-        Featured in bold
-      </p>
-      <p style={{ fontFamily: 'Questrial' }}>Some text</p>
-    </div>
-  );
-};
-
 function App() {
   return (
     <ThemeProvider theme={theme}>
@@ -27,7 +12,7 @@ function App() {
         <Normalize />
         <GlobalStyle />
         <Routes>
-          <Route path="/" element={<MockHome />} />
+          <Route path="/" />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
