42d7ba882ba763bc6ee258c1203c7bb56af5b00e
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-24 17:08:20 -0300

    Create initial mockProducts

---
+++ src/assets/data/mockProducts.js
@@ -0,0 +1,56 @@
+import brownJacket from '../images/products/BrownJacket.webp';
+import classyDress from '../images/products/Classy Dress.webp';
+import colorCombo from '../images/products/Colorful Combo.webp';
+
+const catalog = [
+  {
+    id: 1,
+    title: {
+      en: 'Cool Jacket',
+      pt: 'Jaqueta Descolada',
+    },
+    imageURL: brownJacket,
+    price: '$89,99',
+    sizes: {
+      en: ['S', 'M', 'L'],
+      pt: ['P', 'M', 'G'],
+    },
+    colors: ['brown', 'black'],
+  },
+  {
+    id: 2,
+    title: {
+      en: 'Classy Dress',
+      pt: 'Vestido Elegante',
+    },
+    imageURL: classyDress,
+    price: '$89,99',
+    sizes: {
+      en: ['S', 'M', 'L'],
+      pt: ['P', 'M', 'G'],
+    },
+    colors: ['black', 'grey', 'beige'],
+  },
+  {
+    id: 3,
+    title: {
+      en: 'Colorful Combo',
+      pt: 'Conjundo Colorido',
+    },
+    imageURL: colorCombo,
+    price: '$89,99',
+    sizes: {
+      en: ['S', 'M'],
+      pt: ['P', 'M'],
+    },
+    colors: ['yellow', 'blue', 'red'],
+  },
+];
+
+export function getFeatured() {
+  const featuredIDs = [1, 2, 3];
+  return JSON.stringify(
+    // Discard all products that are NOT at Featured
+    catalog.filter((product) => !featuredIDs.includes(product.id))
+  );
+}
