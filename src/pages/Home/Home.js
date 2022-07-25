efb31ee646d5a21a7872881b4d01631b25ce17a5
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-25 16:29:16 -0300

    Make Home responsible for fetching Featured data
      • It will get the products lists with a query from firestore in the
      future.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,19 @@
 import Featured from './Featured';
 import HeroIntro from './HeroIntro';
 import { getFeatured } from '../../assets/data/mockProducts';
+import { useEffect, useState } from 'react';
 
 export function Home() {
+  const [featured, setFeatured] = useState([]);
+
+  useEffect(() => {
+    setFeatured(JSON.parse(getFeatured()));
+  }, []);
+
   return (
     <main>
       <HeroIntro />
-      <Featured productList={getFeatured()} />
+      <Featured productList={featured} />
     </main>
   );
 }
