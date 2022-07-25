fcf79a6493bab9c200f945603aa7308b0b560aeb
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-25 17:30:13 -0300

    Set styling to FeaturedCard

--- src/pages/Home/Featured/Featured.styled.js
+++ src/pages/Home/Featured/Featured.styled.js
@@ -1,8 +1,33 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-export const FeaturedContainer = styled.section``;
+export const FeaturedContainer = styled.section`
+  height: 480px;
+  background-color: lightblue;
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: scroll;
+`;
 
 export const FeaturedTitle = styled.h2`
   font-family: ${(props) => props.theme.fonts.subheadline};
   font-weight: 300;
+`;
+
+export const StyledCard = styled(Link)`
+  flex-shrink: 0;
+  width: 300px;
+  margin: 5% 2.5%;
+  height: 90%;
+
+  background-color: gray;
+  background-image: url('${(props) => props.image}');
+  background-size: cover;
+
+  display: grid;
+  place-items: end end;
+
+  h3 {
+    color: white;
+  }
 `;
