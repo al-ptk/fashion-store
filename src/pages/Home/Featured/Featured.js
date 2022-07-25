15003f761a067606f931fac2871efacbd3134864
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-25 16:50:23 -0300

    Create FeaturedCard component

--- src/pages/Home/Featured/Featured.js
+++ src/pages/Home/Featured/Featured.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { FeaturedContainer, FeaturedTitle } from './Featured.styled';
+import { Link } from 'react-router-dom';
+import {
+  FeaturedContainer,
+  FeaturedTitle,
+  StyledCard,
+} from './Featured.styled';
 
 export function Featured({ productList }) {
   return (
     <>
       <FeaturedTitle>Featured:</FeaturedTitle>
-      <FeaturedContainer></FeaturedContainer>
+      <FeaturedContainer>
+        {productList.map((elem) => (
+          <FeaturedCard key={elem.id} {...elem} />
+        ))}
+      </FeaturedContainer>
     </>
   );
 }
+
+const FeaturedCard = (props) => {
+  const { imageURL, title, id } = props;
+  return (
+    <Link to={`/products/${id}`}>
+      <StyledCard image={imageURL}>
+        <h3>{title.en}</h3>
+      </StyledCard>
+    </Link>
+  );
+};
