817ea2ce127e9155fcde37d5d645d8a3b7e9351f
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-28 17:43:03 -0300

    Add title to Catalog page

--- src/pages/Catalog/Catalog.js
+++ src/pages/Catalog/Catalog.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import Heading2 from '../../components/Heading2';
 
 export default function Catalog(props) {
-  const params = useParams();
-  console.log(params);
+  const location = useLocation();
+  const filterTitle = location.search.match(/=(.*)/)[1];
 
-  return <div>lul</div>;
+  return (
+    <div>
+      <Heading2>Search for: {filterTitle}</Heading2>
+    </div>
+  );
 }
