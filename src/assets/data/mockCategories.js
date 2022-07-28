a098256af7787039c706f9e8b1f7213514581af5
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-28 16:06:40 -0300

    Create mockCategories

---
+++ src/assets/data/mockCategories.js
@@ -0,0 +1,8 @@
+export const mockCategories = [
+  ['catgory1', '?filter=category1'],
+  ['catgory2', '?filter=category2'],
+  ['catgory3', '?filter=category3'],
+  ['catgory4', '?filter=category4'],
+  ['catgory5', '?filter=category5'],
+  ['catgory6', '?filter=category6'],
+];
