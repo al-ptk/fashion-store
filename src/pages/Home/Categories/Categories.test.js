cb9986aab4cdd0a29e48f4e8d0fe8f9a4ce8db78
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-28 16:03:09 -0300

    Add tests for links

--- src/pages/Home/Categories/Categories.test.js
+++ src/pages/Home/Categories/Categories.test.js
@@ -1,6 +1,7 @@
-import App from '../../../App';
 import '@testing-library/jest-dom';
 import { screen, render } from '@testing-library/react';
+import App from '../../../App';
+import Categories from './index';
 
 describe('Categories', () => {
   it('renders', () => {
@@ -8,5 +9,18 @@ describe('Categories', () => {
     expect(
       screen.getByRole('heading', { name: /Categories/i })
     ).toBeInTheDocument();
+  });
+
+  it.each([
+    ['catgory1', '?filter=category1'],
+    ['catgory2', '?filter=category2'],
+    ['catgory3', '?filter=category3'],
+    ['catgory4', '?filter=category4'],
+    ['catgory5', '?filter=category5'],
+    ['catgory6', '?filter=category6'],
+    ['catalog', '?filter='],
+  ])('%s points to the right page', (linkTitle, linkPath) => {
+    render(<App />);
+    expect(screen.getByRole('link', { name: linkTitle })).toBeInTheDocument();
   });
 });
