96b8d895a1c00650771d3a1f38855a57286b02de
Alan Patrick <alan.patrick.fv@gmail.com>
2022-07-28 18:12:21 -0300

    Pass test of categories leading to proper pages

--- src/assets/data/mockCategories.js
+++ src/assets/data/mockCategories.js
@@ -1,10 +1,10 @@
 const mockCategories = [
-  { title: 'catgory1', path: 'catalog/?filter=category1' },
-  { title: 'catgory2', path: 'catalog/?filter=category2' },
-  { title: 'catgory3', path: 'catalog/?filter=category3' },
-  { title: 'catgory4', path: 'catalog/?filter=category4' },
-  { title: 'catgory5', path: 'catalog/?filter=category5' },
-  { title: 'catgory6', path: 'catalog/?filter=category6' },
+  { title: 'category1', path: 'catalog/?filter=category1' },
+  { title: 'category2', path: 'catalog/?filter=category2' },
+  { title: 'category3', path: 'catalog/?filter=category3' },
+  { title: 'category4', path: 'catalog/?filter=category4' },
+  { title: 'category5', path: 'catalog/?filter=category5' },
+  { title: 'category6', path: 'catalog/?filter=category6' },
 ];
 
 export function getCategories() {
--- src/pages/Home/Categories/Categories.test.js
+++ src/pages/Home/Categories/Categories.test.js
@@ -1,20 +1,35 @@
 import '@testing-library/jest-dom';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../../../App';
+import Categories from './index';
 import { getCategories } from '../../../assets/data/mockCategories';
+import { MemoryRouter } from 'react-router-dom';
+
+const setup = () =>
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
 
 describe('Categories', () => {
   it('renders', () => {
-    render(<App />);
+    setup();
     expect(
       screen.getByRole('heading', { name: /Categories/i })
     ).toBeInTheDocument();
   });
 
-  it.each(getCategories())('$title points to the right page', (linkProps) => {
-    render(<App />);
-    expect(
-      screen.getByRole('link', { name: linkProps.title })
-    ).toBeInTheDocument();
-  });
+  it.each([...getCategories()])(
+    '$title points to the right page',
+    (linkProps) => {
+      setup();
+      const link = screen.getByRole('link', { name: linkProps.title });
+      userEvent.click(link);
+      const pattern = new RegExp('search for: ' + linkProps.title, 'i');
+      const title = screen.getByRole('heading', { name: pattern });
+      expect(title).toBeInTheDocument();
+    }
+  );
 });
